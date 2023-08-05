e921d6edc1ea0b1277a5c09ef8bbf17c2be1c985
Nnamdi Iwenofu <my_nnamdiiwenofu@yahoo.co.uk>
2023-08-05 10:21:08 -0500

    first commit

---
+++ demo-11/demo-11/src/test/java/hello/world/demo1/Demo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.world.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
